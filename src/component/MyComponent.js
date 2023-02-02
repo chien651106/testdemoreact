a6829ec1d0907cf0325be98fe792bc5ff41916f3
Nguyen Dinh Chien <chien651998@gmail.com>
2023-02-02 16:59:47 +0700

    123

---
+++ src/component/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component{
+    //jsx
+    render(){
+        return(
+            <div>abc</div>
+        )
+    }
+}
+export default MyComponent;
