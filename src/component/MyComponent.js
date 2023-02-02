2aba41e18788e1f5592c43890d227bc50ef7ec57
Nguyen Dinh Chien <chien651998@gmail.com>
2023-02-02 17:47:17 +0700

    123

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-class MyComponent extends React.Component{
-    //jsx
-    render(){
-        return(
-            <div>abc</div>
-        )
-    }
+class MyComponent extends React.Component {
+  state = {
+    name: "eric",
+    address: "Ha Noi",
+    age: "25",
+  };
+  //jsx
+  render() {
+    return (
+        <div>
+            My name is {this.state.name} I im from {this.state.address}
+        </div>
+    );
+  }
 }
 export default MyComponent;
